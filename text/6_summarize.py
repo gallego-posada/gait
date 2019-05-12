3c664d91f176e199ed89f6bc59aaf38a09231922
Ankit Vani <a@nevitus.org>
2019-05-12 18:20:48 -0400

    Do things by Kp

--- text/6_summarize.py
+++ text/6_summarize.py
@@ -11,18 +11,22 @@ from torch import nn, optim
 from torch.nn import functional as F
 
 from renyi import mink_sim_divergence
+import utils
 
 
 def print_summary(words, probs, embs):
     p = torch.tensor(np.array(probs, dtype=np.float32)[None, ...])
-    norm = torch.norm(embs, p=2, dim=1, keepdim=True)
-    cosine = (embs @ embs.t()) / norm / norm.t()
+    # norm = torch.norm(embs, p=2, dim=1, keepdim=True)
+    # cosine = (embs @ embs.t()) / norm / norm.t()
+    # K = torch.exp(1.0 * (cosine - 1))  # 2 to increase scale
 
-    K = torch.exp(1.0 * (cosine - 1))  # 2 to increase scale
+    dist = utils.batch_pdist(embs, embs)
+    K = torch.exp(-10 * dist)
+    plt.imshow(K)
+    plt.colorbar()
+    plt.show()
+
     Kp = K @ p[0]
-    # plt.imshow(K)
-    # plt.colorbar()
-    # plt.show()
 
     q_logits = (10 + torch.log(p) + torch.randn_like(p)).requires_grad_()
     # q_logits = (10 + torch.log(p)).requires_grad_()
@@ -33,20 +37,21 @@ def print_summary(words, probs, embs):
     step = 0
     while step < 10000 and not converged:
         q = F.softmax(q_logits, dim=1)
-        div = mink_sim_divergence(K, p, q)
+        div = mink_sim_divergence(K, p, q, use_inv=False)
         div_item = div.item()
         if step % 2000 == 0:
             print('Step %d: %0.4f' % (step, div_item))
         recent.append(div_item)
         div.backward()
         q_optimizer.step()
-        if np.array(recent).mean() < 0.001:
+        if np.array(recent).mean() < 0.0001:
             converged = True
         step += 1
     q = q[0]
-    inp = sorted(list(zip(q, words)), reverse=True)
+    Kq = K @ q
+    inp = sorted(list(zip(Kq, words)), reverse=True)
     print('\nSummary:', ['%s %0.2f' % (w, p * 100) for (p, w) in inp])
-    return q, Kp
+    return Kq, Kp
 
 
 if __name__ == '__main__':
@@ -78,24 +83,33 @@ if __name__ == '__main__':
 
         q, Kp = print_summary(words, probs, embs)
 
-        bar_indices = sorted(list(zip(probs, range(len(feature_index)))), reverse=True)
-        bar_data = list(zip(*[(i, p * 100) for (i, (p, w)) in enumerate(bar_indices)]))
-        plt.bar(*bar_data, width=1.0, alpha=0.5)
-
-        plot_indices = [q[w] for (_, w) in bar_indices]
-        bar_data = list(zip(*[(i, p.item() * 100) for (i, p) in enumerate(plot_indices)]))
-        plt.bar(*bar_data, width=1.0, alpha=0.5)
-        plt.show()
-
         bar_indices = sorted(list(zip(Kp, range(len(feature_index)))), reverse=True)
-        plot_indices = [probs[w] for (_, w) in bar_indices]
-        bar_data = list(zip(*[(i, p * 100) for (i, p) in enumerate(plot_indices)]))
+        bar_data = list(zip(*[(i, p.item() * 100) for (i, (p, w)) in enumerate(bar_indices)]))
         plt.bar(*bar_data, width=1.0, alpha=0.5)
 
         plot_indices = [q[w] for (_, w) in bar_indices]
         bar_data = list(zip(*[(i, p.item() * 100) for (i, p) in enumerate(plot_indices)]))
         plt.bar(*bar_data, width=1.0, alpha=0.5)
         plt.show()
+
+        # bar_indices = sorted(list(zip(probs, range(len(feature_index)))), reverse=True)
+        # bar_data = list(zip(*[(i, p * 100) for (i, (p, w)) in enumerate(bar_indices)]))
+        # plt.bar(*bar_data, width=1.0, alpha=0.5)
+
+        # plot_indices = [q[w] for (_, w) in bar_indices]
+        # bar_data = list(zip(*[(i, p.item() * 100) for (i, p) in enumerate(plot_indices)]))
+        # plt.bar(*bar_data, width=1.0, alpha=0.5)
+        # plt.show()
+
+        # bar_indices = sorted(list(zip(Kp, range(len(feature_index)))), reverse=True)
+        # plot_indices = [probs[w] for (_, w) in bar_indices]
+        # bar_data = list(zip(*[(i, p * 100) for (i, p) in enumerate(plot_indices)]))
+        # plt.bar(*bar_data, width=1.0, alpha=0.5)
+
+        # plot_indices = [q[w] for (_, w) in bar_indices]
+        # bar_data = list(zip(*[(i, p.item() * 100) for (i, p) in enumerate(plot_indices)]))
+        # plt.bar(*bar_data, width=1.0, alpha=0.5)
+        # plt.show()
 
         print("===============")
         print('\n')
