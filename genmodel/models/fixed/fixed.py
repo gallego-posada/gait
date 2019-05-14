fix unbiased gradients

genmodel/models/fixed/fixed.py
@@ -5,6 +5,7 @@ from torch import nn, optim
 from torch.nn import functional as F
 
 from pylego.misc import LinearDecay
+from pylego import ops
 
 from ..basefixed import BaseFixed
 
@@ -29,6 +30,13 @@ class Decoder(nn.Module):
             nn.LeakyReLU(),
             nn.Linear(hidden_size, output_size)
         )
+        # self.fc = nn.Sequential(
+        #     nn.Linear(z_size, z_size * 7 * 7),
+        #     ops.View(-1, z_size, 7, 7),
+        #     ops.ResNet(z_size, [(2, 128, 1), (2, 64, -2), (2, 32, -2), (1, 1, 1)], norm=nn.BatchNorm2d,
+        #                skip_last_norm=True),
+        #     ops.View(-1, 28 * 28)
+        # )
 
     def forward(self, z):
         return torch.sigmoid(self.fc(z))
@@ -51,7 +59,8 @@ class FixedModel(BaseFixed):
         model = Fixed(28 * 28, flags.h_size, flags.z_size)
         optimizer = optim.Adam(model.parameters(), lr=flags.learning_rate, betas=(flags.beta1, flags.beta2))
         super().__init__(model, flags, optimizer=optimizer, *args, **kwargs)
-        uniform = torch.ones(1, flags.batch_size, device=self.device)
+        self.batch_size = flags.batch_size // 2
+        uniform = torch.ones(1, self.batch_size, device=self.device)
         self.uniform = uniform / uniform.sum()
         self.alpha_decay = LinearDecay(flags.alpha_decay_start, flags.alpha_decay_end, flags.alpha_initial, flags.alpha)
         self.kernel = gaussian_kernel(self.flags.kernel_sigma)
@@ -60,6 +69,8 @@ class FixedModel(BaseFixed):
         x_gen = forward_ret
         x = labels.view_as(x_gen)
         alpha = self.alpha_decay.get_y(self.get_train_steps())
-        Dyx = renyi.renyi_mixture_divergence(self.uniform, x, self.uniform, x_gen, self.kernel, alpha)
-        Dxx = renyi.renyi_mixture_divergence(self.uniform, x_gen, self.uniform, x_gen, self.kernel, alpha)
-        return (2 * Dyx) - Dxx
+        Dyx = renyi.renyi_mixture_divergence(self.uniform, x[:self.batch_size], self.uniform, x_gen[:self.batch_size],
+                                             self.kernel, alpha)
+        Dyy = renyi.renyi_mixture_divergence(self.uniform, x_gen[:self.batch_size], self.uniform,
+                                             x_gen[self.batch_size:], self.kernel, alpha)
+        return (2 * Dyx) - Dyy