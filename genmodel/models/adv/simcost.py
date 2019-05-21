8d14a417da8b6e9255acacab335b2425b2488335
Ankit Vani <a@nevitus.org>
2019-05-20 20:50:00 -0400

    Initial simcost code

---
+++ genmodel/models/adv/simcost.py
@@ -0,0 +1,44 @@
+import contextlib
+
+import torch
+from torch import autograd
+from torch.nn import functional as F
+
+from .arch import Discriminator, Generator
+from ..baseadv import BaseAdversarial
+
+
+class SimilarityCostModel(BaseAdversarial):
+
+    def __init__(self, flags, *args, **kwargs):
+        generator = Generator(flags.z_size)
+        discriminator = Discriminator()
+        super().__init__(flags, generator, discriminator, *args, **kwargs)
+
+    def loss_function(self, forward_ret, labels=None):
+        x_gen, x_real = forward_ret
+        if self.debug:
+            debug_context = autograd.detect_anomaly()
+        else:
+            debug_context = contextlib.nullcontext()
+        with debug_context:
+            d_p = self.disc(x_real)
+            d_q = self.disc(x_gen)
+
+        if self.train_disc():
+            if self.flags.gan_loss == 'bce':
+                loss = F.binary_cross_entropy_with_logits(d_p, torch.ones_like(d_p)) + \
+                    F.binary_cross_entropy_with_logits(d_q, torch.zeros_like(d_q))
+            elif self.flags.gan_loss == 'wgan':
+                grad_penalty = self.gradient_penalty(x_real, x_gen, context=debug_context)
+                loss = -d_p.mean() + d_q.mean() + (10.0 * grad_penalty) + 1e-3 * (d_p ** 2).mean()
+            self.d_loss = loss.item()
+        else:
+            if self.flags.gan_loss == 'bce':
+                loss = F.binary_cross_entropy_with_logits(d_p, torch.zeros_like(d_p)) + \
+                    F.binary_cross_entropy_with_logits(d_q, torch.ones_like(d_q))
+            elif self.flags.gan_loss == 'wgan':
+                loss = d_p.mean() - d_q.mean()
+            self.g_loss = loss.item()
+
+        return loss, self.g_loss, self.d_loss
