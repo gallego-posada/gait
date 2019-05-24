2deb5fb969d2baa90f0870500b2e01f5770c04da
Ankit Vani <a@nevitus.org>
2019-05-24 01:46:53 -0400

    update config to reflect paper

--- nonnegative_fixed.py
+++ nonnegative_fixed.py
@@ -64,7 +64,7 @@ if __name__ == '__main__':
                     model = Model(alpha=alpha, size=size, half_empty=half_empty, n=n).cuda()
                     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, betas=(0.9, 0.999))
 
-                    for itr in range(6000):
+                    for itr in range(10000):
                         optimizer.zero_grad()
                         loss = model()
                         if itr % 100 == 0:
